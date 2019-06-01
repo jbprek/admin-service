f68c4c8fbaf688137dbe1f5b411718b4fb3e9ac9
Ioannis Prekezes <jbprek@gmail.com>
2019-06-01 08:19:29 +0100

    first commit

---
+++ src/main/java/forex/bootadmin/BootAdminServerApplication.java
@@ -0,0 +1,14 @@
+package forex.bootadmin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class BootAdminServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BootAdminServerApplication.class, args);
+	}
+	
+}
